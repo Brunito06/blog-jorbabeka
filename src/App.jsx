da2d6d49f1e0d4df4968bc3b6f2b1df1d4d7d796
Bruno Jorba <brujorba@gmail.com>
2024-10-17 19:06:54 -0300

    react structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Pagina de Bruno Jorba y Lucas Bekavac - Proyecto Informatica 6toFM</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
